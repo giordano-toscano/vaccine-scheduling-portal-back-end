cc7c5e1575755c499169de28db5b8c314e3ca38d
giordano-toscano <giordanotoscano@outlook.com>
2022-04-16 02:05:04 -0300

    integration with MongoDB through mongoose, create .env file

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,57 +1,69 @@
-import crypto from "crypto";
-import users from "../model/UserModel.js";
-
-const Controller = {
-    getOne(request, response) {
+import UserModel from "../model/UserModel.js";
+class UserController {
+    async getOne(request, response) {
         const id = request.params.id;
-        const user = users.find((user) => user.id === id);
-        if (user) {
-            return response.send(user);
+        try {
+            const user = await UserModel.findById(id);
+            if (user) {
+                return response.send(user);
+            }
+            response.status(404).send({ message: "User not found" });
+        } catch (error) {
+            //console.error(error.stack);
+            response.status(400).send({ message: "Invalid id" });
         }
-        response.status(404).send({ message: "User not found" });
-    },
+    }
 
-    index(request, response) {
-        response.send({ users });
-    },
+    async index(request, response) {
+        const users = await UserModel.find();
+        response.send(users);
+    }
 
-    remove(request, response) {
+    async remove(request, response) {
         const id = request.params.id;
-        const userIndex = users.findIndex((user) => user.id === id);
-        if (userIndex !== -1) {
-            users.splice(userIndex, 1);
+        const user = await UserModel.findById(id);
+
+        if (user) {
+            await user.remove();
             return response.send({ message: "User Removed" });
         }
         response.status(404).send({ message: "User not found" });
-    },
+    }
 
-    store(request, response) {
-        const { name, city } = request.body;
-        if (name && city) {
-            const user = {
-                id: crypto.randomUUID(),
-                name,
-                city,
-            };
-            users.push(user);
-            return response.send({ message: "User Created !", user });
-        }
+    async store(request, response) {
+        const { name, email, birthDate, schedulingDay, schedulingTime } = request.body;
 
-        response.status(400).send({ message: "Invalid data" });
-    },
+        const user = await UserModel.create({
+            name,
+            email,
+            birthDate,
+            schedulingDay,
+            schedulingTime,
+        });
 
-    update(request, response) {
+        return response.send({ message: "User Created", user });
+    }
+
+    async update(request, response) {
         const id = request.params.id;
-        const { name, city } = request.body;
+        const { name, email, birthDate, schedulingDay, schedulingTime } = request.body;
 
-        const userIndex = users.findIndex((user) => user.id === id);
-        if (userIndex !== -1) {
-            users[userIndex] = { id, name, city };
-            return response.send({ user: users[userIndex] });
-        }
+        const user = await UserModel.findByIdAndUpdate(
+            id,
+            {
+                name,
+                email,
+                birthDate,
+                schedulingDay,
+                schedulingTim,
+            },
+            {
+                new: true,
+            }
+        );
 
-        response.status(404).send({ message: "User not found" });
-    },
-};
+        return response.send(user);
+    }
+}
 
-export default Controller;
+export default UserController;
